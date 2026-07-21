1f88df7d3efb352bf94ccfea36471ac4a7bf54bc
cryptexmarth-code <cryptexmarth@gmail.com>
2026-07-20 23:53:59 -0400

    Update rss-extractor.js

--- scripts/rss-extractor.js
+++ scripts/rss-extractor.js
@@ -4,11 +4,24 @@ const xml2js = require('xml2js');
 
 async function actualizarDatosCompetencia() {
     const canales = [
+        // --- 5 Originales ---
         { nombre: "The Verge", id: "UCBJycsmduvYEL83R_U4JriQ" },
         { nombre: "Linus Tech Tips", id: "UCXuqSBlHAE6Xw-yeJA0Tunw" },
         { nombre: "AuronPlay", id: "UC0V8u0c4OEqCsCH0pKBnEeg" },
         { nombre: "MrBeast", id: "UCX6OQ3DkcsbYNE6H8uQQuVA" },
-        { nombre: "Fazt Code", id: "UCrQ3fJPmgCGIIWdE8_4SrgQ" }
+        { nombre: "Fazt Code", id: "UCrQ3fJPmgCGIIWdE8_4SrgQ" },
+
+        // --- 10 Nuevos en Español e Inglés ---
+        { nombre: "Midudev", id: "UC8LeimukJW82MLSWNVjW3sw" },
+        { nombre: "Dalto", id: "UC9nxKNLyvyaN19UDUf24Pmw" },
+        { nombre: "Ibai", id: "UC02yQWv68_c8wR299E0-lTA" },
+        { nombre: "El Rubius", id: "UCpwgnzQc3mCjWwz_bB1b14Q" },
+        { nombre: "The Wild Project", id: "UCwz8m4nJz8c_p_J07a3lO5w" },
+        { nombre: "DotCSV", id: "UCyBVqKsrW219-X8k-wV-lYw" },
+        { nombre: "MKBHD", id: "UCBJycsmduvYEL83R_U4JriQ" }, // (Nota: MKBHD / Marques Brownlee)
+        { nombre: "Programación ATS", id: "UCT5ZgCeyC5Y4aT_yJ5jZ1sQ" },
+        { nombre: "Devdreaming", id: "UC-lHJZR3Gqxm24_Vd_AJ5Yw" },
+        { nombre: "Pelado Nerd", id: "UCu3774KYXcaUD2pS3C8C9sg" }
     ];
 
     let resultadosGlobales = [];
